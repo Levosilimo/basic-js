70686dea78fa6d3fefad2159355953b73a395e19
Levosilimo <silin.lev@gmail.com>
2022-10-17 17:41:40 +0200

    feat: add solutions

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let sum = 0;
+  do {
+    sum = 0;
+    let digitChars = n.toString(10).split("");
+    digitChars.forEach((char) => {
+      sum+=parseInt(char,10);
+    })
+    n = sum;
+  } while (sum>9);
+
+  return sum;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,61 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    isDirect = false;
+    constructor(isDirect) {
+        this.isDirect=!isDirect;
+    }
+
+    encrypt(message, key) {
+        return this.common(message,key,true);
+    }
+
+    decrypt(encryptedMessage, key) {
+        return this.common(encryptedMessage,key,false);
+    }
+
+    common(message,key,isEncryption){
+        if (typeof message != 'string' || message.length === 0 || typeof key != 'string' || key.length === 0) throw new Error('Incorrect arguments!');
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let resultString = "";
+        let i = 0;
+        let shift = 0;
+        while (i < message.length) {
+            if (/^[A-Z]$/.test(message[i])) {
+                let keyCounter = (i - shift) % key.length;
+                let replacingChar = message.charCodeAt(i) + ((isEncryption?1:-1)*(key.charCodeAt(keyCounter) - 65));
+                if (replacingChar < 65 || replacingChar > 90) replacingChar = (isEncryption? (replacingChar % 91) + 65 : 90 - (64 - (replacingChar % 65)));
+                resultString += String.fromCharCode(replacingChar);
+            } else {
+                resultString += message[i];
+                shift++;
+            }
+            i++;
+        }
+        return this.isDirect? resultString : resultString.split("").reverse().join("");
+    }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
